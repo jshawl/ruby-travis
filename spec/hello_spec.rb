a69fe345f62f404f0d509f2b9acf8919705f8e94
Jesse Shawl <jesse@jshawl.com>
2015-01-19 10:20:20 -0500

    add failing tests

---
+++ spec/hello_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/hello'
+describe Hello do
+  it "can be be instantiated" do
+    @h = Hello.new
+    expect(@h.class).to be(Hello)
+  end
+  it "has a name" do
+    @h = Hello.new("Jesse")
+    expect(@h.name).to be("Jesse")
+  end
+end
